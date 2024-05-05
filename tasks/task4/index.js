bb2a87f88281ea881c5ebb2836ac253f68351448
Oleksandr <lysakova@yahoo.com>
2024-05-05 07:15:41 +0200

    feat(task4): implement debounce function

---
+++ tasks/task4/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+function debounce(fn, delay) {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
+
+function debouncedSearch(query) {
+  // Perform search operation with the query
+  console.log('Searching for:', query);
+}
+
+const debouncedSearchHandler = debounce(debouncedSearch, 1000);
+
+const inputElement = document.getElementById('search-input');
+
+inputElement.addEventListener('input', (event) => {
+  debouncedSearchHandler(event.target.value);
+});
